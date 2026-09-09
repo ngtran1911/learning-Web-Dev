8de85f007c888268d27fa4d14fc361ec16baddee
Tran Vivian Le <giangngoctran.le@tuni.fi>
2026-09-09 15:59:36 +0300

    familiar with Var

---
+++ index.js
@@ -0,0 +1,21 @@
+// var = container store a value 
+
+// declare : let x
+// assign x = 10
+
+// practice declare var and console syntax 
+let famName = "tran";
+let age = 25;
+let email = "ngoctran@gmail.com"
+
+console.log(typeof age);
+console.log(`My name is ${famName}`);
+console.log(`My gmail is ${email}`)
+
+// boolean
+let ontime = true ;
+let IsAstudent = true;
+
+console.log(`Tran is ontime: ${ontime} `)
+console.log(`Enrolled = Student: ${IsAstudent}`)
+
