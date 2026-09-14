695ddf66129b1c0d5e8041417477095d7c0af625
Tran Vivian Le <giangngoctran.le@tuni.fi>
2026-09-14 14:27:51 +0300

    intro to React

---
+++ React/my-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; this is vivian </p>
+        </footer>
+    );
+}
+export default Footer
---
+++ React/my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+      <header>
+        <h1>My websitee</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+                <li>Contact </li>
+            </ul>
+            <hr></hr>
+        </nav>
+      </header>  
+    );
+
+}
+export default Header
